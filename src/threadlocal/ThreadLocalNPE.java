c96c8e3d83d62c0d34cf5af0c7934ac857d77467
yangxuhao <1328243395@qq.com>
2020-12-05 14:55:14 +0800

    first commit

---
+++ src/threadlocal/ThreadLocalNPE.java
@@ -0,0 +1,30 @@
+package threadlocal;
+
+/**
+ * 描述：     TODO
+ */
+public class ThreadLocalNPE {
+
+    ThreadLocal<Long> longThreadLocal = new ThreadLocal<Long>();
+
+    public void set() {
+        longThreadLocal.set(Thread.currentThread().getId());  
+    }
+
+    public long get() {     //public Long get()  因为前面定义的包装类型是Long 拆箱成基本类型 这边会报错
+        return longThreadLocal.get();
+    }
+
+    public static void main(String[] args) {
+        ThreadLocalNPE threadLocalNPE = new ThreadLocalNPE();
+        System.out.println(threadLocalNPE.get());
+        Thread thread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                threadLocalNPE.set();
+                System.out.println(threadLocalNPE.get());
+            }
+        });
+        thread1.start();
+    }
+}
